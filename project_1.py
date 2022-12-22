5678a4c667efa47889dea6d1557db3181291dfc3
ShodmonovZafar <uzbekdasturchisiman@gmail.com>
2022-12-23 03:22:10 +0500

    z

--- project_1.py
+++ project_1.py
@@ -1,8 +1,17 @@
+import requests
 from bs4 import BeautifulSoup
+ 
+page = requests.get('https://www.imdb.com/chart/top/')  # Getting page HTML through request
+soup = BeautifulSoup(page.content, 'html.parser')  # Parsing content using beautifulsoup
+ 
+links = soup.select("table tbody tr td.titleColumn a") # Selecting all of the anchors with titles
+first10 = links[:10] # Keep only the first 10 anchors
+for anchor in first10:
+    print(anchor.text) # Display the innerText of each anchor
 
-def get_product(data:str) -> str:
-    """Get the product name from the data string.
-
+def get_product(data: str) -> str:
+    """
+    Get the product name from the data string.
     Args:
         data (str): The HTML string.
 
@@ -39,10 +48,10 @@ def save_product(product:str) -> None:
         f.write(product)
 
 # Open the file and read the HTML string
-with open('html/product1.html', 'r') as f:
-    data = f.read()
+# with open('html/product1.html', 'r') as f:
+#     data = f.read()
 
 # Get the product name in CSV format
-product_csv=get_product(data) 
+product_csv=get_product(soup.prettify()) 
 # Save the product name to the CSV file
 save_product(product_csv) 
